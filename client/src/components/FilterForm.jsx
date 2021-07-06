4a2cc41165dc2265355f43fe94c84cda9cc192a0
Derek <maloned@uci.edu>
2021-07-05 17:26:53 -0700

    Add search filters that update state of filter form component

--- client/src/components/FilterForm.jsx
+++ client/src/components/FilterForm.jsx
@@ -1,9 +1,59 @@
 import React, {useState} from 'react';
 
-function FilterForm() {
+function FilterForm(props) {
+  const [open, setOpen] = useState(true)
+  const [values, setValues] = useState({
+    FA: '',
+    Counsel: '',
+    SaleDateStart: '',
+    SaleDateEnd: '',
+    IssuerType: '',
+    DebtType: '',
+    RefundAmtMin: '',
+    RefundAmtMax: '',
+    SaleType: '',
+    Issuer: ''
+  })
 
+  const handleChange = (event) => {
+    let name = event.target.name
+    setValues({...values, [name]: event.target.value})
+  }
+  // Issuer:			D, F (low)
+// Underwriter:		D, F (low)
+// Placement Agent:	D, F (low)
+// CAB Flag:		D, F (low)
+// Guarantor:		D, F (low)
+// Discl Counsel:		D, F (low)
   return (
-    <div>Test</div>
+    <div>
+      Filter Search:
+      <form className='filter-form'>
+        <label>
+          Financial Advisor: <input type='text' name='FA' value={values.FA} onChange={handleChange}/>
+        </label>
+        <label>
+          Bond Counsel:
+          <input type='text' name='Counsel' value={values.Counsel} onChange={handleChange}/>
+        </label>
+        <label>
+          Sale Date Range: <input type='date' name='SaleDateStart' value={values.SaleDateStart} onChange={handleChange}/> to <input type='date' name='SaleDateEnd' value={values.SaleDateEnd} onChange={handleChange}/>
+        </label>
+        <label>
+           Issuer Type: <input type='text' name='IssuerType' value={values.IssuerType} onChange={handleChange}/>
+        </label>
+        <label>
+           Sale Type: <input type='text' name='SaleType' value={values.SaleType} onChange={handleChange}/>
+        </label>
+        <label>
+           Refund Amount Range: <input type='number' name='RefundAmtMin' value={values.RefundAmtMin} onChange={handleChange}/> to <input type='number' name='RefundAmtMax' value={values.RefundAmtMax} onChange={handleChange}/>
+        </label>
+        <label>
+           Debt Type: <input type='text' name='DebtType' value={values.DebtType} onChange={handleChange}/>
+        </label >
+
+      </form>
+    </div>
   )
 }
 
