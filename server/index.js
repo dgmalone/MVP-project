982142f972aea4acb7be5f9bf6bca5c597fddc3d
Derek <maloned@uci.edu>
2021-07-22 16:12:51 -0700

    Slight code clean up

--- server/index.js
+++ server/index.js
@@ -12,17 +12,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('/Bonds', (req, res) => {
-  //console.log(req.body)
-  //console.log(req.query)
-
-  //res.send(req.body)
   APIcalls.fetchData(JSON.parse(req.query.filters))
     .then(results => {
-      //console.log(results.data)
       res.send(results.data)
     })
     .catch(err => {
-      //console.log(err)
       res.status(400)
       res.send(err)
     }
@@ -32,7 +26,6 @@ app.get('/Bonds', (req, res) => {
 })
 
 app.post('/Bonds', (req, res) => {
-  //console.log(req.body)
   sfDb.saveFilter(req.body, (err, docs) => {
     if (err) {
       res.status(400);
@@ -44,7 +37,6 @@ app.post('/Bonds', (req, res) => {
 })
 
 app.get('/Favorites', (req, res) => {
-  //console.log(req.query);
   sfDb.getFavs(req.query.userName, (err, docs) => {
     if (err) {
       res.status(400);
@@ -56,7 +48,6 @@ app.get('/Favorites', (req, res) => {
 })
 
 app.put('/Favorites/Delete', (req, res) => {
-  //console.log(req.query, req.body);
   sfDb.deleteFav(req.body.id, (err, docs) => {
     if (err) {
       res.status(400);
@@ -78,7 +69,6 @@ app.put('/Favorites/UpdateName', (req, res) => {
   })
 })
 app.put('/Favorites/UpdateFilters', (req, res) => {
-  //console.log(req.query, req.body);
   sfDb.updateFavFilter(req.body, (err, docs) => {
     if (err) {
       res.status(400);
@@ -88,7 +78,7 @@ app.put('/Favorites/UpdateFilters', (req, res) => {
     }
   })
 })
-// format the date and $ data to look better on the front end before sending it
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
